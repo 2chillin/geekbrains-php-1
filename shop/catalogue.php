98aabca151bc4220de713d3d180ca4a8a3b629d9
2chillin <admin@pkbuh.ru>
2019-01-24 18:54:40 +0300

    PHP 2 Домашнее задание к уроку №4

--- shop/catalogue.php
+++ shop/catalogue.php
@@ -4,25 +4,36 @@
     include('header.php');
 ?>
 	<h1 class="main">КАТАЛОГ ТОВАРОВ</h1>
-<?php
-    foreach ($products as $product) {
-?>
-	<div class="cat-product">
-		<h3><?=$product['name']?></h3>
-		<div class="image">
+    <div id="catalogue-all">
+    <?php
+        foreach ($products as $product) {
+            if ($a++ >= 3) {
+                break;
+            }
+    ?>
+        <div class="cat-product">
+            <h3><?=$product['name']?></h3>
+            <div class="image">
+                <a href="catalogue-item.php?id=<?=$product['id']?>">
+                    <img width="150" src="<?=$product['image_url']?>">
+                </a>
+            </div>
+            <p class="price"><?=$product['price']?> руб.</p>
             <a href="catalogue-item.php?id=<?=$product['id']?>">
-                <img width="150" src="<?=$product['image_url']?>">
+                <button class="shop-button">ПОДРОБНО</button>
+            </a>
+            <br><br>
+            <a href="#" onclick="item(<?=$product['id']?>)">
+                <button class="shop-button">В КОРЗИНУ</button>
             </a>
         </div>
-        <p class="price"><?=$product['price']?> руб.</p>
-        <a href="catalogue-item.php?id=<?=$product['id']?>">
-            <button class="shop-button">ПОДРОБНО</button>
-        </a>
-        <br><br>
-        <a href="#" onclick="item(<?=$product['id']?>)">
-            <button class="shop-button">В КОРЗИНУ</button>
+    <?php } ?>
+    </div>
+    <br style="clear: both;">
+    <div id="button-more">
+        <a href="#" onclick="more()">
+            <button class="shop-button">Загрузить еще..</button>
         </a>
     </div>
-<?php } ?>
 
 <?php include('footer.php'); ?>
